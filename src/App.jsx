51d2350aa3c9dc1667087f2c3629505af09c15cd
rohitsingh79 <rohitprataps4@gmail.com>
2021-07-29 20:16:09 +0530

    profile website skeleton deleted file for creating website skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,13 @@
-import React from "react";
-import { itemsListNested, itemListProp } from "./sampleData";
-import Accordian from "./AccordianContainer";
-import Accordianchild1 from "./Accordianchild1";
-import flattenDeep from "lodash/flattenDeep";
-import ContentTemplate from "./ContentTemplate";
+import React, { useEffect, useRef, useState } from "react";
+import Button from "@material-ui/core/Button";
+//import ContentTemplate from "./ContentTemplate";
 
 export default function App() {
-  const items1 = itemsListNested;
-  const items2 = itemListProp;
-
-  const [[singleChild]] = items1[0].child;
-  const [[doubleChild]] = items2[0].child;
-  console.log(`1st use case ${singleChild}`);
-  const actualChildren = flattenDeep(singleChild["child"]);
-  console.log(actualChildren);
-  //console.log(doubleChild);
-
-  // const [[doubleChild1]] = singleChild["child"];
-  // const [[doubleChild2]] = doubleChild["child"];
-
   return (
     <div className="App">
-      Iam just testing stuff
-      <ContentTemplate />
+      <Button variant="contained" color="primary">
+        Hello World resume bana do bhai
+      </Button>
     </div>
   );
 }
---
+++ src/style.js
@@ -0,0 +1,10 @@
+import styled, { css } from "styled-components";
+
+const Scroll = styled.div`
+  width: 100%;
+  height: 100px;
+  padding: 20px;
+  background-color: blue;
+`;
+
+export { Scroll };
