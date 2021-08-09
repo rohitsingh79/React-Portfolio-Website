5ae213a3302fcdb24b09417cd38c8774908f2c08
rohitsingh79 <rohitprataps4@gmail.com>
2021-08-09 10:43:50 +0530

    added website

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,70 @@
-import React, { useEffect, useRef, useState } from "react";
-import Button from "@material-ui/core/Button";
-//import ContentTemplate from "./ContentTemplate";
+import React, { useState } from "react";
+//import Button from "@material-ui/core/Button";
+// import PassImage from "../public/passport.jpeg";
+
+import {
+  Header,
+  RoundDiv,
+  DivWrapper,
+  Description,
+  GridContainer
+} from "./style";
 
 export default function App() {
+  const [hoverState, setHoverState] = useState(false);
+  console.log("hello");
   return (
-    <div className="App">
-      <Button variant="contained" color="primary">
-        Hello World resume bana do bhai
-      </Button>
-    </div>
+    <DivWrapper>
+      <div className="App">
+        <Header className="headerText">
+          <RoundDiv
+            hoverState={hoverState}
+            onMouseEnter={() => setHoverState(true)}
+            onMouseLeave={() => setHoverState(false)}
+          >
+            {/* <img
+          src={PassImage}
+        /> */}
+          </RoundDiv>
+        </Header>
+        <Description>
+          ROHIT PRATAP SINGH
+          <br />
+          SOFTWARE ENGINEER @ LOWES
+        </Description>
+        <GridContainer>
+          <div className="GridChild">
+            <svg height="110" width="300">
+              <polygon
+                points="50 3,100 28,100 75, 50 100,3 75,3 25"
+                stroke="black"
+                fill="yellow"
+                stroke-width="5"
+              />
+            </svg>
+          </div>
+          <div className="GridChild">
+            <svg height="110" width="300">
+              <polygon
+                points="50 3,100 28,100 75, 50 100,3 75,3 25"
+                stroke="black"
+                fill="yellow"
+                stroke-width="5"
+              />
+            </svg>
+          </div>
+          <div className="GridChild">
+            <svg height="110" width="300">
+              <polygon
+                points="50 3,100 28,100 75, 50 100,3 75,3 25"
+                stroke="black"
+                fill="yellow"
+                stroke-width="5"
+              />
+            </svg>
+          </div>
+        </GridContainer>
+      </div>
+    </DivWrapper>
   );
 }
--- src/style.js
+++ src/style.js
@@ -1,10 +1,68 @@
 import styled, { css } from "styled-components";
 
-const Scroll = styled.div`
+const DivWrapper = styled.div`
   width: 100%;
-  height: 100px;
-  padding: 20px;
-  background-color: blue;
+  height: 100%;
+  position: relative;
+  text-align: center;
+  background-color: gray;
 `;
 
-export { Scroll };
+const Header = styled.div`
+  width: 100%;
+  height: 200px;
+  padding: 20px;
+  background-color: #008b8b;
+  position: relative;
+  text-align: center;
+  justify-content: center;
+
+  .headerText {
+    text-align: center;
+    color: yellow;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+  }
+`;
+
+const RoundDiv = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: MediumSeaGreen;
+  border-radius: 50%;
+  position: absolute;
+  top: 55%;
+  left: 42%;
+  padding-top: ${(props) => props.topPadding}px;
+
+  ${({ hoverState }) =>
+    hoverState === true
+      ? css`
+          background-color: blue;
+        `
+      : css`
+      background-color:cyan: ;
+      `};
+`;
+
+const Description = styled.div`
+  margin-top: 120px;
+`;
+
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: auto auto auto;
+  grid-gap: 50px;
+  background-color: #2196f3;
+  padding: 50px;
+  margin-top: 10%;
+  .GridChild {
+    background-color: rgba(255, 255, 255, 0.8);
+    text-align: center;
+    padding: 100px 20px;
+    font-size: 30px;
+  }
+`;
+
+export { Header, RoundDiv, DivWrapper, Description, GridContainer };
