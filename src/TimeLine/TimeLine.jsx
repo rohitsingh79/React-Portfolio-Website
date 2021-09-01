f07f253c920e9860a5d7174d3cebb27ef57bf4d6
rohitsingh79 <rohitprataps4@gmail.com>
2021-09-01 10:50:35 +0530

    summary

--- src/TimeLine/TimeLine.jsx
+++ src/TimeLine/TimeLine.jsx
@@ -1,57 +1,67 @@
 import React from "react";
-import { TimeLineWrapper } from "./style";
+import {
+  TimeLineWrapper,
+  ColumnWrapper,
+  ColumnContainer,
+  TimeLineBar
+} from "./style";
 
 const TimeLine = () => {
   return (
-    <TimeLineWrapper>
-      <div className="TimeLineChild">
-        <h3 style={{ paddingLeft: "20px" }}>
-          Internship at Juniper 2019-present
-        </h3>
-        <p style={{ paddingLeft: "20px" }}>
-          I recently have joined the Juniper Networs as a Software
-          Developer-Intern in the DevOps team. My major part of the work has
-          been into the field of creating sustainable and flexible CI/CD
-          (Continuous Integration & Continuous Development) pipelines which
-          enables developers at Juniper to do rapid development. I am also
-          exploring the role of container technology like Docker and
-          container-orchrastation technologies like K8s, Swarm to create
-          industry standard production environments.
-        </p>
-      </div>
+    <ColumnWrapper>
+      <ColumnContainer>
+        <TimeLineBar />
+        <TimeLineWrapper>
+          <div className="TimeLineChild childHeight">
+            <h3 style={{ paddingLeft: "20px" }}>
+              Internship at Juniper 2019-present
+            </h3>
+            <p style={{ paddingLeft: "20px" }}>
+              I recently have joined the Juniper Networs as a Software
+              Developer-Intern in the DevOps team. My major part of the work has
+              been into the field of creating sustainable and flexible CI/CD
+              (Continuous Integration & Continuous Development) pipelines which
+              enables developers at Juniper to do rapid development. I am also
+              exploring the role of container technology like Docker and
+              container-orchrastation technologies like K8s, Swarm to create
+              industry standard production environments.
+            </p>
+          </div>
 
-      <div className="TimeLineChild">
-        <h3 style={{ paddingLeft: "20px" }}>
-          Internship at Juniper 2019-present
-        </h3>
-        <p style={{ paddingLeft: "20px" }}>
-          I recently have joined the Juniper Networs as a Software
-          Developer-Intern in the DevOps team. My major part of the work has
-          been into the field of creating sustainable and flexible CI/CD
-          (Continuous Integration & Continuous Development) pipelines which
-          enables developers at Juniper to do rapid development. I am also
-          exploring the role of container technology like Docker and
-          container-orchrastation technologies like K8s, Swarm to create
-          industry standard production environments.
-        </p>
-      </div>
+          <div className="TimeLineChild">
+            <h3 style={{ paddingLeft: "20px" }}>
+              Internship at Juniper 2019-present
+            </h3>
+            <p style={{ paddingLeft: "20px" }}>
+              I recently have joined the Juniper Networs as a Software
+              Developer-Intern in the DevOps team. My major part of the work has
+              been into the field of creating sustainable and flexible CI/CD
+              (Continuous Integration & Continuous Development) pipelines which
+              enables developers at Juniper to do rapid development. I am also
+              exploring the role of container technology like Docker and
+              container-orchrastation technologies like K8s, Swarm to create
+              industry standard production environments.
+            </p>
+          </div>
 
-      <div className="TimeLineChild">
-        <h3 style={{ paddingLeft: "20px" }}>
-          Internship at Juniper 2019-present
-        </h3>
-        <p style={{ paddingLeft: "20px" }}>
-          I recently have joined the Juniper Networs as a Software
-          Developer-Intern in the DevOps team. My major part of the work has
-          been into the field of creating sustainable and flexible CI/CD
-          (Continuous Integration & Continuous Development) pipelines which
-          enables developers at Juniper to do rapid development. I am also
-          exploring the role of container technology like Docker and
-          container-orchrastation technologies like K8s, Swarm to create
-          industry standard production environments.
-        </p>
-      </div>
-    </TimeLineWrapper>
+          <div className="TimeLineChild">
+            <h3 style={{ paddingLeft: "20px" }}>
+              Internship at Juniper 2019-present
+            </h3>
+            <p style={{ paddingLeft: "20px" }}>
+              I recently have joined the Juniper Networs as a Software
+              Developer-Intern in the DevOps team. My major part of the work has
+              been into the field of creating sustainable and flexible CI/CD
+              (Continuous Integration & Continuous Development) pipelines which
+              enables developers at Juniper to do rapid development. I am also
+              exploring the role of container technology like Docker and
+              container-orchrastation technologies like K8s, Swarm to create
+              industry standard production environments.
+            </p>
+          </div>
+        </TimeLineWrapper>
+      </ColumnContainer>
+    </ColumnWrapper>
   );
 };
 
--- src/TimeLine/style.js
+++ src/TimeLine/style.js
@@ -5,19 +5,72 @@ const ColumnWrapper = styled.div`
   flex-direction: row;
 `;
 
-const TimeLineWrapper = styled.div`
+const ColumnContainer = styled.div`
   display: grid;
   margin-top: 100px;
-  grid-template-rows: 150px 150px 150px;
-  grid-gap: 10px;
+  margin-left: 40px;
+  grid-template-columns: 5px 1250px;
+  grid-gap: 30px;
   padding: 40px;
+  height: 450px;
+`;
+
+const TimeLineBar = styled.div`
+  background-color: #f2f3f7;
+`;
+
+const TimeLineWrapper = styled.div`
+  display: grid;
+  grid-template-rows: auto auto auto;
+  grid-gap: 10px;
 
   .TimeLineChild {
     text-align: left;
     font-size: 15px;
     background-color: #f2f3f7;
-    width: 1250px;
+    position: relative;
+    height: 200px;
+
+    &::before {
+      content: "";
+      position: absolute;
+      width: 20px;
+      height: 20px;
+      background-color: white;
+      border: 4px solid #ff9f55;
+      border-radius: 50%;
+      z-index: 1;
+      left: -48px;
+    }
+
+    &::after {
+      content: "";
+      height: 0;
+      position: absolute;
+      top: 5px;
+      width: 0;
+      z-index: 1;
+      left: -8px;
+      border: medium solid #f2f3f7;
+      border-width: 10px 10px 10px 0;
+      border-color: transparent #f2f3f7 transparent transparent;
+    }
   }
 `;
 
-export { TimeLineWrapper, ColumnWrapper };
+// const PseudoCheck = styled.div`
+//   background-color: grey;
+//   left: -20px;
+//   &::after {
+//     content: "";
+//     position: absolute;
+//     top: 22px;
+//     width: 0;
+//     z-index: 1;
+//     border: medium solid white;
+//     border-width: 10px 0 10px 10px;
+//     border-color: transparent transparent transparent grey;
+//   }
+// `;
+
+export { TimeLineWrapper, ColumnWrapper, ColumnContainer, TimeLineBar };
