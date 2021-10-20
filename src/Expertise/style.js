757f7804c3abcdd29ba97abed95d8edf26810772
Rohit Pratap Singh <rohitprataps4@gmail.com>
2021-10-20 14:19:52 +0530

    initial commit

--- src/Expertise/style.js
+++ src/Expertise/style.js
@@ -20,9 +20,44 @@ const GridContainer = styled.div`
     justify-content: center;
     margin-bottom: 80px;
     transform: translateY(40px);
-    @media only screen and (max-width: 600px) {
-    }
   }
 `;
 
-export { GridContainer };
+const GridChild = styled.div`
+  background-color: #fff;
+  box-shadow: 0px 0px 56px -8px rgb(0 0 0 / 17%);
+  height: 230px;
+  width: auto;
+  /* display: flex;
+  align-items: center;
+  justify-content: center; */
+  padding-left: 15px;
+  padding-right: 15px;
+  position: relative;
+`;
+
+const HexagonMainWrapper = styled.div`
+  width: 100px;
+  height: 100px;
+  position: relative;
+  left: 36%;
+  top: -22%;
+`;
+
+const SvgIconMainWrapper = styled.div`
+  position: relative;
+  top: -70%;
+`;
+
+const ContentWrapper = styled.div`
+  position: relative;
+  top: -20%;
+`;
+
+export {
+  GridContainer,
+  GridChild,
+  HexagonMainWrapper,
+  SvgIconMainWrapper,
+  ContentWrapper
+};
